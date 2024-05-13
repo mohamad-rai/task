bfd0a46d07742074d462f9f768f4602338e45094
Mohammad Reza Ahmadi <0918.mohamad@gmail.com>
2024-05-13 16:42:45 +0330

    local auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { RefreshJwtStrategy } from './strategies/refreshToken.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, JwtService, UserService, LocalStrategy],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    UserService,
+    LocalStrategy,
+    RefreshJwtStrategy,
+  ],
   imports: [
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
-        expiresIn: '1h',
+        expiresIn: '10m',
       },
     }),
   ],
+  exports: [JwtModule],
 })
 export class AuthModule {}
